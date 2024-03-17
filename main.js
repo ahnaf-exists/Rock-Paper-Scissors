fbdd96bb6118a68a1dc9d0d9b37b75754778edb1
Ahnaf Tahmid <ahnaf.officialprofile@gmail.com>
2024-03-17 16:09:08 +0600

    Version 2.2

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const currentMatch = document.querySelector(".current-match");
 const currentStats = document.querySelector(".current-stats");
 let intervalId;
+let pauseId;
+let isAutoplaying = false;
 let score = JSON.parse(localStorage.getItem("score"));
 if (score === null) {
     score = {win: 0, lose: 0, draw: 0};
@@ -12,15 +14,17 @@ const autoplayBtn = document.querySelector(".auto")
 function autoplay() {
     if (autoplayBtn.innerText === "Autoplay") {
         autoplayBtn.innerText = "Stop Playing";
+        isAutoplaying = true;
         let move;
 
         intervalId = setInterval(function startAutoplay(){
             move = generateRandomMove();
             playMove(move);
-        }, 100);
+        }, 800);
     } else {
         clearInterval(intervalId);
         autoplayBtn.innerText = "Autoplay";
+        isAutoplaying = false;
     }
 }
 
@@ -62,23 +66,36 @@ function displayDraw() {
 }
 
 function playMove(userMove) {
-    const computerMove = generateRandomMove();
-    currentMatch.innerText = `${userMove} VS ${computerMove}`;
-    if (userMove === computerMove) {
-        displayDraw();
-    } else if (userMove === "Rock" && computerMove === "Paper") {
-        displayLose();
-    } else if (userMove === "Rock" && computerMove === "Scissors") {
-        displayWin();
-    } else if (userMove === "Paper" && computerMove === "Rock") {
-        displayLose();
-    } else if (userMove === "Paper" && computerMove === "Scissors") {
-        displayWin();
-    } else if (userMove === "Scissors" && computerMove === "Rock") {
-        displayLose();
-    } else if (userMove === "Scissors" && computerMove === "Paper") {
-        displayWin();
+    function play() {
+        const computerMove = generateRandomMove();
+            currentMatch.innerText = `${userMove} VS ${computerMove}`;
+            if (userMove === computerMove) {
+                displayDraw();
+            } else if (userMove === "Rock" && computerMove === "Paper") {
+                displayLose();
+            } else if (userMove === "Rock" && computerMove === "Scissors") {
+                displayWin();
+            } else if (userMove === "Paper" && computerMove === "Rock") {
+                displayLose();
+            } else if (userMove === "Paper" && computerMove === "Scissors") {
+                displayWin();
+            } else if (userMove === "Scissors" && computerMove === "Rock") {
+                displayLose();
+            } else if (userMove === "Scissors" && computerMove === "Paper") {
+                displayWin();
+            }
+            localStorage.setItem("score", JSON.stringify(score));
+            scoreHTML.innerText = `Win: ${score.win}, Lose: ${score.lose}, Draw: ${score.draw}`;
     }
-    localStorage.setItem("score", JSON.stringify(score));
-    scoreHTML.innerText = `Win: ${score.win}, Lose: ${score.lose}, Draw: ${score.draw}`;
+    if (!isAutoplaying) {
+        isAutoplaying = true;
+        currentMatch.innerText = `${userMove} VS ...`;
+        pauseId = setInterval(function() {
+            play();
+            clearInterval(pauseId);
+            isAutoplaying = false;
+        }, 800)
+    } else {
+        play();
+    }
 }
